79261507954801d888f44d1de0f7469f1e414a9b
goldds <100039628+goldds@users.noreply.github.com>
2022-12-15 16:21:31 +0000

    size changing

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -14,28 +14,28 @@ function makeGrid() {
 //parameters fetching
   const hieght = urlParams.get('height');
   const width = urlParams.get('width');
-  const size = urlParams.get('size')
-  console.log(size)
-  const styleDef = `height:${size}; width:${size};`
-
+  const size = urlParams.get('size');
+  console.log(size);
+  const  defstyle = `height:${size}; width:${size};`;
+  size.output = defstyle;
 //grid creation
-  var row = "<tr>" + ((`<td class = 'pixel' style= ${styleDef} id = ></td>`).repeat(width)) + "</tr>";
+  var row = "<tr>" + ((`<td class = 'pixel' style='' id = ></td>`).repeat(width)) + "</tr>";
   var grid = row.repeat(hieght);
 
   var pixelCanavs = document.getElementById("pixelCanvas");
 
   pixelCanavs.insertAdjacentHTML("afterbegin", grid);
 
-  var pixels = document.getElementsByClassName("pixel")
+  var pixels = document.getElementsByClassName("pixel");
 
  
   function callback(id){
     return function () {
       if(pixels[id].style.backgroundColor == ''){
-        draw(pixels[id],styleDef)
+        draw(pixels[id],);
     }
     else{
-      pixels[id].setAttribute("style", (styleDef + " background-color:") ) 
+      pixels[id].setAttribute("style", ( " background-color:") );
 
     }
   }
@@ -43,17 +43,16 @@ function makeGrid() {
 
 
   for(var i = 0; i <= ((pixels.length)-1  ); i++){
-    pixels[i].setAttribute("id", i)
-
-    pixels[i].addEventListener('click', callback(i))
+    pixels[i].setAttribute("id", i);
+    pixels[i].addEventListener('click', callback(i));
 }}
 
 // grid editing
 
-function draw(pixel,styleDef) {
-  var color = (document.getElementById("colorPicker")).value 
-  var css = styleDef + " background-color:" + color +";"
-  pixel.setAttribute("style", css)
+function draw(pixel) {
+  var color = (document.getElementById("colorPicker")).value;
+  var css =  " background-color:" + color +";";
+  pixel.setAttribute("style", css);
 }
 
 
